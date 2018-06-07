6f4e35bf3f1916c14706146d1e0aaee98cbe6620
Recursif <jillian.dum@gmail.com>
2018-06-07 20:39:14 +0200

    care of the views file and place template inside

--- pokedex/routes/pokemons.js
+++ pokedex/routes/pokemons.js
@@ -1,13 +1,14 @@
-var router = require('express').Router();
+const path = require('path')
+const router = require('express').Router()
 
-var pokemon = require('./../models/pokemon.js')
+const pokemon = require('./../models/pokemon.js')
 
 router.get('/', (req, res) => {
     // populate permet de répérer tout les pokemons plus leurs types associés
     pokemon.find({}).populate('types')
     .then(pokemons => {
         console.log(__dirname)
-        res.render('../view/pokemons/index.html', {pokemons: pokemons})
+        res.render(path.join(__dirname, '../views/pokemons/index.html'), {pokemons: pokemons})
     })
 })
 
