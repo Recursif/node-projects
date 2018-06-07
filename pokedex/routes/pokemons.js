d63fddcb031f00b958a61868b0778087cde62f20
Recursif <jillian.dum@gmail.com>
2018-06-07 20:44:12 +0200

    function pokemon but remember to add studio 3T

--- pokedex/routes/pokemons.js
+++ pokedex/routes/pokemons.js
@@ -7,7 +7,6 @@ router.get('/', (req, res) => {
     // populate permet de répérer tout les pokemons plus leurs types associés
     pokemon.find({}).populate('types')
     .then(pokemons => {
-        console.log(__dirname)
         res.render(path.join(__dirname, '../views/pokemons/index.html'), {pokemons: pokemons})
     })
 })
