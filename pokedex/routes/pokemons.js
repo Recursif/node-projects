ac8d913298c7b495694806f32ddc12ec077efec7
Recursif <jillian.dum@gmail.com>
2018-06-14 10:33:09 +0200

    change form with value

--- pokedex/routes/pokemons.js
+++ pokedex/routes/pokemons.js
@@ -7,20 +7,20 @@ router.get('/', (req, res) => {
     // populate permet de répérer tout les pokemons plus leurs types associés
     Pokemon.find({}).populate('type')
     .then(pokemons => {
-        res.render(path.join(__dirname, '../views/pokemons/index.html'), {pokemons: pokemons})
+        res.render(path.join(__dirname, '../views/pokemons/index.html'), { pokemons: pokemons })
     })
 })
 
 router.get('/:id', (req, res) => {
     Pokemon.findById(req.params.id).populate('types').then(pokemon => {
-        res.render('pokemons/show.html', {pokemon: pokemon})
+        res.render('pokemons/show.html', { pokemon: pokemon })
     },
     err => res.status(500).send(err))
 })
 
 router.get('/new', (req, res) => {
     var pokemon = new Pokemon()
-    res.render('pokemons/edit.html')
+    res.render('pokemons/edit.html', { pokemon: pokemon })
 })
 
 router.get('/edit/:id', (req, res) => {
