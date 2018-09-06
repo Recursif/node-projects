e0b59039daf84c4903a692e2b3224fe8220b4f02
Recursif <jillian.dum@gmail.com>
2018-09-06 16:52:32 +0200

    node docker test

---
+++ node-docker/server.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+
+const port = 8080
+const host = '0.0.0.0'
+
+app.get('/', (req, res) => {
+    res.send('Hello world\n')
+})
+
+app.listen(port, host)
+console.log(`Running on http://${host}:{port}`)
